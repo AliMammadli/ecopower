0db8486641289ebbb7643d91c674dbfec63696b5
Ali Mammadli <nfsavss@icloud.com>
2022-12-10 15:16:18 +0400

    o

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import './styles/slider.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>
+    // </React.StrictMode>
 )
